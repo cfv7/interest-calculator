bde647b261bb7995759f76fcc4020fdf7255e043
cfv7 <hq@colin.red>
2017-04-11 14:21:05 -0400

    finished the drills

--- src/components/interest-calculator.js
+++ src/components/interest-calculator.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {setPrincipal, setInterest, setYears} from '../actions';
 
 // Connect this component
-export default function InterestCalculator(props) {
+export function InterestCalculator(props) {
   return (
     <form className="interest-calculator"
       onSubmit={e => e.preventDefault()}>
       <div className="form-group">
         <label htmlFor="principal">Principal ($)</label>
-        <input type="number" id="principal" value={props.principal}
+        <input onChange={ e => {
+          props.dispatch(setPrincipal(e.target.value))
+          }}type="number" id="principal" value={props.principal}
           min="0" />
       </div>
       <div className="form-group">
         <label htmlFor="interest">Interest rate (%)</label>
-        <input type="number" id="interest" value={props.interest}
+        <input onChange={ e => {
+          props.dispatch(setInterest(e.target.value))
+        }}
+        type="number" id="interest" value={props.interest}
           min="0" max="100" step="0.1" />
       </div>
       <div className="form-group">
         <label htmlFor="years">Years</label>
-        <input type="number" id="years" value={props.years}
+        <input onChange={ e => {
+          props.dispatch(setYears(e.target.value))
+        }}
+        type="number" id="years" value={props.years}
           min="0" max="100" />
       </div>
       <div className="form-group">
@@ -34,3 +44,12 @@ InterestCalculator.defaultProps = {
   years: 0,
   total: 0
 };
+
+const mapStateToProps = state =>  ({
+  principal: state.principal,
+  interest: state.interest,
+  years: state.years,
+  total: state.principal * Math.pow(1 + state.interest / 100, state.years)
+});
+
+export default connect(mapStateToProps)(InterestCalculator)
